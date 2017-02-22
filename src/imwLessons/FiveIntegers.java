f8c24e58d1ed846b4dfadcc1aed2387b07140343
Sokos6 <will.sokolowski@gmail.com>
2017-02-22 15:14:36 -0500

    five int

---
+++ src/imwLessons/FiveIntegers.java
@@ -0,0 +1,29 @@
+package imwLessons;
+
+public class FiveIntegers {
+	public static void main(String[] args) {
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
+		
+		int num1 = 0;
+		int num2 = 0;
+		int num3 = 0;
+		int num4 = 0;
+		int num5 = 0;
+		
+		System.out.println("Enter a number : ");
+		num1 = scanner.nextInt();
+		System.out.println("Enter a second number : ");
+		num2 = scanner.nextInt();
+		System.out.println("Enter a third number : ");
+		num3 = scanner.nextInt();
+		System.out.println("Enter a fourth number : ");
+		num4 = scanner.nextInt();
+		System.out.println("Enter a fifth number : ");
+		num5 = scanner.nextInt();
+		
+		System.out.println(num1 + " " + num2 + " " + num3 + " " + num4 + " " + num5);
+		
+		scanner.close();
+	
+	}
+}
