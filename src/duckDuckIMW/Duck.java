d335beee603de7c7ecfe530a725dfded39aaa7a0
Sokos6 <will.sokolowski@gmail.com>
2017-03-21 17:37:14 -0400

    model duck

--- src/duckDuckIMW/Duck.java
+++ src/duckDuckIMW/Duck.java
@@ -21,5 +21,13 @@ public abstract class Duck {
 	public void swim() {
 		System.out.println("All ducks float, even decoys!");
 	}
+	
+	public void setFlyBehavior(FlyBehavior fb) {
+		flyBehavior = fb;
+	}
+	
+	public void setQuackBehavior(QuackBehavior qb) {
+		quackBehavior = qb;
+	}
 
 }
